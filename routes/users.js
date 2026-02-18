32d4b35127eb7fb28907ad35108e67dc57383bc5
Marie <marie.roche32@orange.fr>
2026-02-18 21:52:16 +0100

    [ADD] route test without env

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,40 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post('/inscription', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (EMAIL_REGEX.test(req.body.email)) {
+
+          User.findOne({ email: req.body.email }).then(data => {
+            if (data === null) {
+              const hash = bcrypt.hashSync(req.body.password, 10);
+              const newToken = uid2(32);
+              const newUser = new User({
+                token: newToken,
+                email: req.body.email,
+                password: hash,
+                created: new Date(),
+              });
+
+              newUser.save().then(data => {
+                res.json({ result: true, token: newToken });
+              });
+
+            } else {
+              res.json({ result: false, error: 'Utilisateur déja enregistré' });
+            }
+          });
+        } else {
+      res.json({ result: false, error: 'Format email invalide' });
+
+    }
+  });
+
 module.exports = router;
