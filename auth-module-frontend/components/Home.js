b43015416d5289ace3f65ce798a35d88f998c7d8
Marie <marie.roche32@orange.fr>
2026-02-18 02:20:05 +0100

    [ADD] simple onclick buttons

--- auth-module-frontend/components/Home.js
+++ auth-module-frontend/components/Home.js
@@ -1,15 +1,41 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 function Home() {
+
+    const [connectionEmail, setConnectionEmail] = useState('');
+    const [connectionPassword, setConnectionPassword] = useState('');
+
+    const [inscriptionEmail, setInscriptionEmail] = useState('');
+    const [inscriptionPassword, setInscriptionPassword] = useState('');
+
+    const [erreurConnection, setErreurConnection] = useState('');
+    const [erreurInscription, setErreurInscription] = useState('');
+
+const SuivantConnection = () => {
+  console.log('yay!');
+}
+
+const SuivantInscription = () => {
+  console.log('whoho!!');
+}
+
+
   return (
     <div>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Connection/Inscription
+          Connection
         </h1>
-        <input placeholder="Nom d'utilisateur"></input>
-        <input placeholder="Mot de passe"></input>
-        <button>Entrer</button>
+        <input onChange={(value) => setConnectionEmail(value)} value={connectionEmail} placeholder="Email"></input>
+        <input onChange={(value) => setConnectionPassword(value)} value={connectionPassword} placeholder="Mot de passe"></input>
+        <button onClick={SuivantConnection}>Se connecter</button>
+        <h1 className={styles.title}>
+            Inscription
+        </h1>
+        <input onChange={(value) => setInscriptionEmail(value)} value={inscriptionEmail} placeholder="Email"></input>
+        <input onChange={(value) => setInscriptionPassword(value)} value={inscriptionPassword} placeholder="Mot de passe"></input>
+        <button onClick={SuivantInscription}>S'inscrire</button>
       </main>
     </div>
   );
