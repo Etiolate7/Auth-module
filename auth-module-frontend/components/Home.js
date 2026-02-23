07bef375f112e7346e6c1c9ab61af80b3c84af37
Marie <marie.roche32@orange.fr>
2026-02-23 22:43:06 +0100

    [ADD} connection button for multiple sessions same user

--- auth-module-frontend/components/Home.js
+++ auth-module-frontend/components/Home.js
@@ -26,6 +26,24 @@ function Home() {
       });
   }
 
+  const SuivantConnexion = () => {
+    fetch('http://localhost:3000/users/connexion', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: inscriptionEmail, password: inscriptionPassword }),
+      credentials: 'include'
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result) {
+            localStorage.setItem('accessToken', data.accessToken);
+            window.location.href = '/main';
+        } else {
+          console.log(data.error)
+          setErreurInscription(data.error)
+        }
+      });
+  }
+
   return (
     <div>
       <main className={styles.main}>
@@ -36,6 +54,7 @@ function Home() {
         <input onChange={(e) => setInscriptionEmail(e.target.value)} value={inscriptionEmail} placeholder="Email"></input>
         <input onChange={(e) => setInscriptionPassword(e.target.value)} value={inscriptionPassword} placeholder="Mot de passe"></input>
         <button onClick={SuivantInscription}>Entrer</button>
+        <button onClick={SuivantConnexion}>Se connecter</button>
       </main>
     </div>
   );
